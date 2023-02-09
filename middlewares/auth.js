e7b2c7d8675fe3b920408f0041953a6065096054
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2023-02-09 18:30:08 +0100

    Début de l'ajout de modification dans le middleware d'authentification

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,13 +2,14 @@ const UnauthorizedError = require("../errors/unauthorized");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   try {
     const token = req.headers["x-access-token"];
     if (!token) {
       throw "not token";
     }
     const decoded = jwt.verify(token, config.secretJwtToken);
+    const user = await User.findById(decoded.id);
     req.user = decoded;
     next();
   } catch (message) {
