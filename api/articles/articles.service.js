987a3c0caa3adb3af043cdfd139c5c2e07c96441
Adrien SCHMIDT <adrien.schmidt7@gmail.com>
2023-02-09 16:26:29 +0100

    Ajout du contrôleur

---
+++ api/articles/articles.service.js
@@ -0,0 +1,17 @@
+const User = require("./articles.schema");
+const bcrypt = require("bcrypt");
+
+class ArticleService {
+    create(data) {
+        const article = new Article(data);
+        return article.save();
+    }
+    update(id, data) {
+        return Article.findByIdAndUpdate(id, data, { new: true });
+    }
+    delete(id) {
+        return Article.deleteOne({ _id: id });
+    }
+}
+
+module.exports = new ArticleService();
